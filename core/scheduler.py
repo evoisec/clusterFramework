bc05554bca669703a4489e9a10ea7044b7e0ebb7
evo <evo.eftimov@isecc.com>
2020-09-07 20:33:43 +0100

    s

--- core/scheduler.py
+++ core/scheduler.py
@@ -2,29 +2,3 @@ import sys
 import sampletask as mod
 
 mod.trainMLModel("HMM_1")
-
-sys.exit(0)
-
-
-
-
-module = __import__(module_name)
-my_class = getattr(module, class_name)
-instance = my_class()
-
-
-class DynamicImporter:
-    """"""
-
-    # ----------------------------------------------------------------------
-    def __init__(self, module_name, class_name):
-        """Constructor"""
-        module = __import__(module_name)
-        my_class = getattr(module, class_name)
-        instance = my_class()
-        print
-        instance
-
-
-if __name__ == "__main__":
-    DynamicImporter("decimal", "Context")
---
+++ core/scheduler3.py
@@ -0,0 +1,23 @@
+
+
+
+module = __import__(module_name)
+my_class = getattr(module, class_name)
+instance = my_class()
+
+
+class DynamicImporter:
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self, module_name, class_name):
+        """Constructor"""
+        module = __import__(module_name)
+        my_class = getattr(module, class_name)
+        instance = my_class()
+        print
+        instance
+
+
+if __name__ == "__main__":
+    DynamicImporter("decimal", "Context")

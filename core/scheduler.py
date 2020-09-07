9c907276cb40ad9972f9e01c50e4418b0f955cad
evo <evo.eftimov@isecc.com>
2020-09-07 18:17:07 +0100

    Initial commit

---
+++ core/scheduler.py
@@ -0,0 +1,26 @@
+# importing module.py file
+import callbackmlone as mod
+
+# running the welcome method
+mod.trainMLModel("HMM_1")
+
+module = __import__(module_name)
+my_class = getattr(module, class_name)
+instance = my_class()
+
+
+class DynamicImporter:
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self, module_name, class_name):
+        """Constructor"""
+        module = __import__(module_name)
+        my_class = getattr(module, class_name)
+        instance = my_class()
+        print
+        instance
+
+
+if __name__ == "__main__":
+    DynamicImporter("decimal", "Context")

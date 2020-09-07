27712fde319fe910e25c112666678404c05b5623
evo <evo.eftimov@isecc.com>
2020-09-07 20:18:33 +0100

    s

--- core/scheduler2.py
+++ core/scheduler2.py
@@ -13,3 +13,15 @@ plugin = plugin_module.Plugin("HMM", key=123)
 result = plugin.execute(5, 3)
 
 print(result)
+
+PLUGIN_NAME = "sampletask22"
+
+plugin_module = importlib.import_module(PLUGIN_NAME, ".")
+
+print(plugin_module)
+
+plugin = plugin_module.Plugin("SVM", key=333)
+
+result = plugin.execute(5, 3)
+
+print(result)

222a1ac8b8d90a2fcfa43cac6f8a2987799516c0
evo <evo.eftimov@isecc.com>
2021-02-21 19:20:50 +0000

    s

--- core/sub.py
+++ core/sub.py
@@ -1,5 +1,5 @@
+import sys
 from google.cloud import pubsub_v1
-
 
 class WorkflowStateMachine:
 
@@ -45,25 +45,60 @@ def process_topic(subscriber, subscription_path, workflow_state_machine):
     return workflow_state_machine
 
 
+def sdl_event_generator():
+    return 0
+
 
 project_id = "studied-client-297916"
 
 upstream_subscription_id = "upstream-central-orchestrator"
 sdl_subscription_id = "sdl-central-orchestrator"
 sfl_subscription_id = "sfl-central-orchestrator"
+command_subscription_id = "command-channel-central-orchestrator"
 
 upstream_subscriber = pubsub_v1.SubscriberClient()
+sdl_subscriber = pubsub_v1.SubscriberClient()
 sfl_subscriber = pubsub_v1.SubscriberClient()
+command_subscriber = pubsub_v1.SubscriberClient()
 
 upstream_subscription_path = upstream_subscriber.subscription_path(project_id, upstream_subscription_id)
+sdl_subscription_path = sdl_subscriber.subscription_path(project_id, sdl_subscription_id)
 sfl_subscription_path = sfl_subscriber.subscription_path(project_id, sfl_subscription_id)
+command_subscription_path = command_subscriber.subscription_path(project_id, command_subscription_id)
 
+# Enforces strictly sequential message processing (in combination with the used synchronous pull pubsub client type)
 NUM_MESSAGES = 1
 
 workflow_state_machine = WorkflowStateMachine()
 
+
+##############################################################################################
+# This is the main processing loop. It cycles through each topic, processes the messages/events and
+# updates the Workflow State Machine. It exits if it receives a command from the command topic
+##############################################################################################
 while True:
+
+    # Business Logic for Upstream EoD Events
 
     workflow_state_machine = process_topic(upstream_subscriber, upstream_subscription_path, workflow_state_machine)
 
+    # Business Logic for SDL Events
+
+    workflow_state_machine = process_topic(sdl_subscriber, sdl_subscription_path, workflow_state_machine)
+
+    # Business Logic for SFL Events
+
     workflow_state_machine = process_topic(sfl_subscriber, sfl_subscription_path, workflow_state_machine)
+
+    # Business Logic for the SDL Event Generator (currently a CRON job every 5 min)
+
+    sdl_event_generator()
+
+    # Process the command topic of the Central Orchestrator and if necessary terminate/exit the Central Orchestrator
+
+    workflow_state_machine = process_topic(sfl_subscriber, sfl_subscription_path, workflow_state_machine)
+
+    if workflow_state_machine.is_terminate_command():
+        sys.exit(0)
+
+

5b34c56ee2c779b1a99e5d3a6bf1f9a4e0714265
evo <evo.eftimov@isecc.com>
2021-02-26 23:01:50 +0000

    s

--- core/pub.py
+++ core/pub.py
@@ -4,13 +4,12 @@ from google.cloud import pubsub_v1
 project_id = "studied-client-297916"
 
 upstream_topic_id = "argo"
-sfl_topic_id = "sfl-hub"
 
 upstream_publisher = pubsub_v1.PublisherClient()
 upstream_topic_path = upstream_publisher.topic_path(project_id, upstream_topic_id)
 
-for n in range(1, 5):
-    data = "Upstream Msg number {}".format(n)
+for n in range(1, 4):
+    data = "Message number {}".format(n)
     # Data must be a bytestring
     data = data.encode("utf-8")
     # Add two attributes, origin and username, to the message
@@ -19,21 +18,7 @@ for n in range(1, 5):
     )
     print(future.result())
 
-print(f"Published Upstream messages with custom attributes to {upstream_topic_path}.")
+print(f"Published internal CC messages with custom attributes to {upstream_topic_path}.")
 
 ################################################################################################
 
-sfl_publisher = pubsub_v1.PublisherClient()
-sfl_topic_path = sfl_publisher.topic_path(project_id, sfl_topic_id)
-
-for n in range(1, 5):
-    data = "SFL Msg number {}".format(n)
-    # Data must be a bytestring
-    data = data.encode("utf-8")
-    # Add two attributes, origin and username, to the message
-    future = sfl_publisher.publish(
-        sfl_topic_path, data, origin="workflow-orchestrator", event_name="SFL_WORKFLOW_COMPLETION"
-    )
-    print(future.result())
-
-print(f"Published SFL messages with custom attributes to {sfl_topic_path}.")
--- core/sub.py
+++ core/sub.py
@@ -1,16 +1,13 @@
-import sys
+import sys, time
 from google.cloud import pubsub_v1
 
+# this is just a skeleton/placeholder for a Workflow State Machine (if needed) - for the moment the role of such
+# will be played by the CO Audit Log
 class WorkflowStateMachine:
 
-  state = "Completed"
-  terminate_command = False
+    def __init__(self, message_id):
+        self.gcp_message_id = message_id
 
-  def is_terminate_command(self):
-      return self.terminate_command
-
-  def terminate(self):
-      self.terminate_command = True
 
 
 ############################################################################################################
@@ -59,6 +56,8 @@ def process_topic(subscriber, subscription_path, workflow_state_machine):
         # Dedup the Event Message against CO Audit Log, here also skip any further processing - if dedup conditions encountered
         ############################################################################
 
+        # ACK immediately if duplicate or already processed and go back to the message consumption step
+
         print("Deduping the Event Message against the CO Audit Log")
 
         #ToDO: Persist the Event Message to the CO Audit Log
@@ -75,11 +74,16 @@ def process_topic(subscriber, subscription_path, workflow_state_machine):
         # Acknowledge the Event Message to GCP PUBSUB
         ############################################################################
 
-        # check whether ACK can still be done and of not, then consume the message again and ACK it
+        # simulates business processing so the time for ACK expires - to be removed, used only for testing/simulation
+        # default topic time for ACK is 10 sec
+        #time.sleep(MESSAGE_ACK_TIME + 10)
 
+        # will not throw error is the ACK time for the message has expired - so here we are just making best effort to ACK
+        # the dedup step which will be invoked during the next itteration, will ensure/double check that the message is ACKed
         request = {"subscription": subscription_path, "ack_ids": ack_ids}
         subscriber.acknowledge(request)
-        print( f"Received and acknowledged {len(response.received_messages)} messages from {subscription_path}." )
+
+        print( f"Received, logged in CO Audit Table and made best effort to acknowledge {len(response.received_messages)} messages from {subscription_path}." )
 
         #ToDO: Perform business processing for the messages in the current topic and then update / process the Workflow State MAchine based on the received events/messages
 
@@ -91,17 +95,22 @@ def process_topic(subscriber, subscription_path, workflow_state_machine):
         print("Mapping Event Message to CC Argo Workflow Name")
 
         ############################################################################
-        # Trigger CC Argo Workflow
+        # Trigger CC Argo Workflow by invoking the "argo submit" as a shell command
         ############################################################################
         print("Triggering CC Argo Workflow")
 
         ############################################################################
-        # Keep checking the CO Audit Log whether the CC Workflow had started - loop for a configurable interval
+        # Keep checking (for awhile) the CO Audit Log whether the CC Workflow had started - loop for a configurable interval
+        # if necessary raise alert
+        # this can be enhanced further by streaming the real-time log of the CC Argo Workflow with the "argo submit --log" command
+        # and parsing and scanning the log for error conditions
         ############################################################################
         print("Checking whether the CC Argo Workflow has started")
 
 
     # ToDO: perform final operation on the State Machine before return
+
+
 
     return workflow_state_machine
 
@@ -115,7 +124,12 @@ cc_topic_subscription_path = cc_topic_subscriber.subscription_path(project_id, c
 # Enforces strictly sequential message processing (in combination with the used synchronous pull pubsub client type)
 NUM_MESSAGES = 1
 
-workflow_state_machine = WorkflowStateMachine()
+# the default ACK time for the topic
+MESSAGE_ACK_TIME = 10
+
+gcp_message_id = None
+
+workflow_state_machine = WorkflowStateMachine(None)
 
 
 ##############################################################################################

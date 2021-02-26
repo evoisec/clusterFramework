ea7aa6b927232930c3a497215c01d1144c8d79c4
evo <evo.eftimov@isecc.com>
2021-02-26 19:22:54 +0000

    s

--- core/sub.py
+++ core/sub.py
@@ -15,8 +15,9 @@ class WorkflowStateMachine:
 
 ############################################################################################################
 # Note, while processing the messages/events in a topic, here we can loop until all messages are processed
-# still one message at a time ie Strictly Sequentially or we can process just one message per each invokation
+# still one message at a time ie Strictly Sequentially or we can process just one message per each invocation
 # of this function
+# The current implementation processes strictly one event message per invocation
 ############################################################################################################
 def process_topic(subscriber, subscription_path, workflow_state_machine):
 
@@ -39,43 +40,61 @@ def process_topic(subscriber, subscription_path, workflow_state_machine):
                     value = received_message.message.attributes.get(key)
                     print(f"{key}: {value}")
 
+        #ToDO: Dedup the Event Message
+
+        ############################################################################
+        # Dedup the Event Message against CO Audit Log, here also skip any further processing - if dedup conditions encountered
+        ############################################################################
+
+        print("Deduping the Event Message against the CO Audit Log")
+
+        #ToDO: Persist the Event Message to the CO Audit Log
+
+        ############################################################################
+        # Persist the Event Message in CO Audit Log conditional on only if it doesnt already exist there
+        # When done here, it prevents Race Conditions between CO and the CC Workflows when updating the CO Audit Log
+        # This step corresponds to Workflow State = Event Message Received by CO and Stored in CO Audit Log
+        ############################################################################
+
+        print("Persisting the Event Message to the CO Audit Log")
+
+        ############################################################################
+        # Acknowledge the Event Message to GCP PUBSUB
+        ############################################################################
+        request = {"subscription": subscription_path, "ack_ids": ack_ids}
+        subscriber.acknowledge(request)
+        print( f"Received and acknowledged {len(response.received_messages)} messages from {subscription_path}." )
 
         #ToDO: Perform business processing for the messages in the current topic and then update / process the Workflow State MAchine based on the received events/messages
 
 
-        subscriber.acknowledge(
-            request={"subscription": subscription_path, "ack_ids": ack_ids}
-        )
+        ############################################################################
+        # Map Event Message to CC Argo Workflow Name
+        ############################################################################
 
-        print(
-            f"Received and acknowledged {len(response.received_messages)} messages from {subscription_path}."
-        )
+        print("Mapping Event Message to CC Argo Workflow Name")
+
+        ############################################################################
+        # Trigger CC Argo Workflow
+        ############################################################################
+        print("Triggering CC Argo Workflow")
+
+        ############################################################################
+        # Keep checking the CO Audit Log whether the CC Workflow had started - loop for a configurable interval
+        ############################################################################
+        print("Checking whether the CC Argo Workflow has started")
+
 
     # ToDO: perform final operation on the State Machine before return
 
     return workflow_state_machine
 
 
-def sdl_event_generator(workflow_state_machine):
-    return 0
-
-
 project_id = "studied-client-297916"
+cc_topic_subscription_id = "upstream-central-orchestrator"
 
-upstream_subscription_id = "upstream-central-orchestrator"
-sdl_subscription_id = "sdl-central-orchestrator"
-sfl_subscription_id = "sfl-central-orchestrator"
-command_subscription_id = "command-channel-central-orchestrator"
-
-upstream_subscriber = pubsub_v1.SubscriberClient()
-sdl_subscriber = pubsub_v1.SubscriberClient()
-sfl_subscriber = pubsub_v1.SubscriberClient()
-command_subscriber = pubsub_v1.SubscriberClient()
-
-upstream_subscription_path = upstream_subscriber.subscription_path(project_id, upstream_subscription_id)
-sdl_subscription_path = sdl_subscriber.subscription_path(project_id, sdl_subscription_id)
-sfl_subscription_path = sfl_subscriber.subscription_path(project_id, sfl_subscription_id)
-command_subscription_path = command_subscriber.subscription_path(project_id, command_subscription_id)
+cc_topic_subscriber = pubsub_v1.SubscriberClient()
+cc_topic_subscription_path = cc_topic_subscriber.subscription_path(project_id, cc_topic_subscription_id)
 
 # Enforces strictly sequential message processing (in combination with the used synchronous pull pubsub client type)
 NUM_MESSAGES = 1
@@ -84,16 +103,14 @@ workflow_state_machine = WorkflowStateMachine()
 
 
 ##############################################################################################
-# This is the main processing loop. It cycles through each topic, processes the messages/events and
-# updates the Workflow State Machine. It exits if it receives a command from the command topic
+# This is the main Central Orchestrator (CO) processing loop. It cycles through each topic (currently only one), processes the messages/events and
+# updates the CO Audit Log and Workflow State Machine. It exits if it receives a command from the command topic
 ##############################################################################################
 while True:
 
-    # Business Logic for Upstream EoD Events
+    print("################### Start of New Event Message Processing Cycle #########################################################")
 
-    print("#########################################################################################################")
-
-    workflow_state_machine = process_topic(upstream_subscriber, upstream_subscription_path, workflow_state_machine)
+    workflow_state_machine = process_topic(cc_topic_subscriber, cc_topic_subscription_path, workflow_state_machine)
 
 
 

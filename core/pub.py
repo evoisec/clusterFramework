d35a6b09e66bdfcab670148b47d3b396300bcf20
evo <evo.eftimov@isecc.com>
2021-02-28 17:53:56 +0000

    s

--- core/pub.py
+++ core/pub.py
@@ -8,18 +8,17 @@ upstream_publisher = pubsub_v1.PublisherClient()
 upstream_topic_path = upstream_publisher.topic_path(project_id, upstream_topic_id)
 
 for n in range(1, 4):
-    data = """{"event_name":"ledger_refresh_completion","source":"workflow_1","is_cascade":true,"reporting_date":"2021-3-3"}"""
+    data = """{"event_name":"ledger_refresh_completion_event","source":"workflow_1","is_cascade":true,"reporting_date":"2021-3-3"}"""
     #     Data must be a bytestring
     data = data.encode("utf-8")
     # Add attributes to the message - note, these are GCP Messahe Header Attributes
     future = upstream_publisher.publish(
-         upstream_topic_path, data, origin="workflow-orchestrator", event_name="ledger_refresh_completion", event_seq_number = str(n)
+         upstream_topic_path, data, origin="workflow-orchestrator", event_name="ledger_refresh_completion_event", event_seq_number = str(n)
     )
     print(future.result())
 
 print(f"Published internal CC messages with custom attributes to {upstream_topic_path}.")
 
-################################################################################################
 
 
 
